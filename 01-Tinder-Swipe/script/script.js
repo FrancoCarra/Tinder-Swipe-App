543b4611d8cc34ce352675f7961c2c98b35967ea
franco carra <francocarra6@gmail.com>
2024-02-26 23:39:56 -0500

    SWIPE-0.02

---
+++ 01-Tinder-Swipe/script/script.js
@@ -0,0 +1,94 @@
+let isAnimating = false
+let pullDeltaX = 0 //Card distance animating
+const DECISION_THRESHOLD = 75
+
+
+function startDrag(event) {
+    if (isAnimating) return
+
+    //get the first article element
+    const actualCard = event.target.closest('article')
+
+    //get initial position of mouse or finger
+    const startX = event.pageX ?? event.touches[0].pageX
+
+    //w mouse
+    document.addEventListener('mousemove', onMove)
+    document.addEventListener('mouseup', onEnd)
+
+    //w touch
+    document.addEventListener('touchmove', onMove, { passive: true })
+    document.addEventListener('touchend', onEnd,{ passive: true })
+
+    function onMove(event){ 
+    //current position of mouse or finger
+        const currentX = event.pageX ?? event.touches[0].pageX
+    //the distance between the initial and current position
+        pullDeltaX = currentX - startX
+
+        //if we dont have distance
+        if (pullDeltaX == 0)return
+        //change the flag to indicate we are animating
+        isAnimating = true
+        //calculate the rotation of the card using the distance
+        const deg = pullDeltaX / 14
+
+        //apply the transformation to the card
+        actualCard.style.transform = `translateX(${pullDeltaX}px) rotate(${deg}deg)`
+        actualCard.style.cursor = 'grabbing' //cursor grabbing
+    }
+
+    function onEnd(event) {
+        //remove the events listeners
+        document.removeEventListener('mousemove', onMove)
+        document.removeEventListener('mouseup', onEnd)
+        document.removeEventListener('touchmove', onMove)
+        document.removeEventListener('touchend', onEnd)
+
+        // //TODO: TO REMOVE AS WE`RE DOING THIS OTHER WAY
+
+        // //reset the flag
+        // isAnimating = false
+        // //reset the distance
+        // pullDeltaX = 0
+        // //reset the transformation
+        // actualCard.style.transform = 'none'
+        // //reset the cursor
+        // actualCard.style.cursor = 'grab'
+
+
+        //KNOW IF THE USER TAKES A DECISION
+
+        const decisionMade = math.abs(pullDeltaX) >= DECISION_THRESHOLD
+        if (decisionMade) {
+            const goRight = pullDeltaX >= 0
+            const goLeft = !goRight
+            
+        //add class according to decision
+        actualCard.classList.add(goRight ? 'go-right' : 'go-left')
+        //eliminate the element when the transition is finish
+        actualCard.addEventListener('transitionend', () => {
+            actualCard.remove()
+        })
+        }else{
+            actualCard.classList.add('reset')
+            actualCard.classList.remove('go-right', 'go-left')
+        }
+
+        //reset the variables
+        actualCard.addEventListener('transitionend', () => {
+            actualCard.removeAttribute('style')
+            actualCard.classList.remove('reset')
+
+            pullDeltaX = 0;
+            isAnimating = false; 
+        })
+
+    }
+}
+
+
+
+document.addEventListener('mousedown', startDrag)
+document.addEventListener('touchstart', startDrag, { passive: true })
+
